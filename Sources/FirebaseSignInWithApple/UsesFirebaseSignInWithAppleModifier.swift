898deccc5da3c412e826dd19893b796a504a4170
Alex Nagy <rebeloperalpar@gmail.com>
2024-05-08 14:13:13 +0300

    1

--- Sources/FirebaseSignInWithApple/UsesFirebaseSignInWithAppleModifier.swift
+++ Sources/FirebaseSignInWithApple/UsesFirebaseSignInWithAppleModifier.swift
@@ -15,7 +15,7 @@ struct UsesFirebaseSignInWithAppleModifier: ViewModifier {
         content
             .environment(\.firebaseSignInWithApple, controller)
             .onAppear {
-                DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
                     controller.startListeningToAuthChanges()
                 }
             }
