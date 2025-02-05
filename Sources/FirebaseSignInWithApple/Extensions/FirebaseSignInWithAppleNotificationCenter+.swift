3f574961485596db9435a05bdf44ac12efbf9089
alexatgetcalvinapp <alex@getcalvin.app>
2025-02-05 19:04:57 +0200

    ASAuthorizationError

--- Sources/FirebaseSignInWithApple/Extensions/FirebaseSignInWithAppleNotificationCenter+.swift
+++ Sources/FirebaseSignInWithApple/Extensions/FirebaseSignInWithAppleNotificationCenter+.swift
@@ -6,11 +6,43 @@
 //
 
 import Foundation
+import AuthenticationServices
+import FirebaseCore
 
 public extension NotificationCenter {
     static func post(error: Error) {
-        DispatchQueue.main.async {
-            NotificationCenter.default.post(name: Notification.Name.firebaseSignInWithAppleError, object: error.localizedDescription)
+        if let error = error as? ASAuthorizationError {
+            var message: String = ""
+            switch error.code {
+                case .canceled:
+                message = "User canceled sign-in."
+            case .unknown:
+                message = "An unknown error occurred."
+            case .invalidResponse:
+                message = "The response from Apple was invalid."
+            case .notHandled:
+                message = "The authorization request was not handled."
+            case .failed:
+                message = "Apple failed to authorize."
+            case .notInteractive:
+                message = "The authorization request could not be completed interactively."
+            case .matchedExcludedCredential:
+                message = "The credential was previously rejected."
+            case .credentialImport:
+                message = "The credential could not be imported."
+            case .credentialExport:
+                message = "The credential could not be exported."
+            @unknown default:
+                message = "An unknown error occurred."
+            }
+            DispatchQueue.main.async {
+                NotificationCenter.default.post(name: Notification.Name.firebaseSignInWithAppleError, object: message)
+            }
+        } else {
+            DispatchQueue.main.async {
+                NotificationCenter.default.post(name: Notification.Name.firebaseSignInWithAppleError, object: error.localizedDescription)
+            }
         }
+        
     }
 }
