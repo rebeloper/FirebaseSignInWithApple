3798b7a1d69609c7d4bdef2500810f8c53bd8e1b
alexatgetcalvinapp <alex@getcalvin.app>
2025-02-05 14:11:42 +0200

    Firestorable

---
+++ Sources/FirebaseSignInWithApple/Protocols/Firestorable.swift
@@ -0,0 +1,12 @@
+//
+//  Firestorable.swift
+//  FirebaseSignInWithApple
+//
+//  Created by Alex Nagy on 05.02.2025.
+//
+
+import Foundation
+
+public protocol Firestorable {
+    var userUid: String { get set }
+}
